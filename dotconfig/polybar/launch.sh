315dbfe58e28e725f37db60a6c031f09fef360ba
Logwheel1 <107886754+Logwheel1@users.noreply.github.com>
2023-03-02 16:08:39 -0500

    Update launch.sh

--- dotconfig/polybar/launch.sh
+++ dotconfig/polybar/launch.sh
@@ -8,6 +8,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar --config=$HOME/.config/polybar/config.ini bar1 | tee -a /tmp/polybar1.log & disowm
+polybar --config=$HOME/.config/polybar/config.ini bar1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bar launched..."
